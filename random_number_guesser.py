78cf2f3fe1d771c9a5d89c4e783726a22ad2f4a1
espal11 <167619214+espal11@users.noreply.github.com>
2026-07-08 23:31:02 +0200

    Add files via upload

--- random_number_guesser.py
+++ random_number_guesser.py
@@ -1,34 +1,54 @@
 import random
 
+tries = 0
+
 print("welcome to espels number gussing game!")
 print("choose a difficlty")
 print("(1) EASY (2) MEDIUM (3) HARD")
 d = int(input("Your choice: "))
 
 if d == 1:
-    print("EASY")
+    print("EASY 1-10")
     n = random.randint(1, 10)
-    a = int(input("Your guess: "))
-    if n == a:
-        print("you guess was correct!")
-    else:
-        print("your guess were wrong :C")
-        print("the right awnser was:", n)
+    while True:
+        a = int(input("Enter your guess: "))
+        tries += 1
+        if n == a:
+            print("your guess was correct")
+            print("your final socre was:", tries)
+            break
+        elif a > n:
+            print("your guess was to high")
+        elif n > a:
+            print("your guess was to low")
 elif d == 2:
-    print("medium")
-    n = random.randint(1, 25)
-    a = int(input("Your guess: "))
-    if n == a:
-        print("your guess was correct!")
-    else:
-        print("your guess were wrong :C")
-        print("the right awnser was:", n)
-elif d == 3:
-    print("HARD")
+    print("MEDIUM 1-50")
     n = random.randint(1, 50)
-    a = int(input("Your guess: "))
-    if n == a:
-        print("you guess was correct!")
-    else:
-        print("your guess were wrong :C")
-        print("the right awnser was:", n)
+    while True:
+        a = int(input("Enter your guess: "))
+        tries += 1
+        if n == a:
+            print("your guess was correct")
+            print("your final socre was:", tries)
+            break
+        elif a > n:
+            print("your guess was to high")
+        elif n > a:
+            print("your guess was to low")
+else:
+    print("HARD 1-100")
+    n = random.randint(1, 100)
+    while True:
+        a = int(input("Enter your guess: "))
+        tries += 1
+        if n == a:
+            print("your guess was correct")
+            print("your final socre was:", tries)
+            break
+        elif a > n:
+            print("your guess was to high")
+        elif n > a:
+            print("your guess was to low")
+
+
+
